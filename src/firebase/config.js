e156f17918128161d7459356fd6b553d1dd14838
deenadhayalan7 <727721euai014@skcet.ac.in>
2023-06-16 16:14:57 +0530

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAar1YK6KGVuLDYefnfh4DKiSvoof5iArU",
+    authDomain: "carrier-guidance.firebaseapp.com",
+    projectId: "carrier-guidance",
+    storageBucket: "carrier-guidance.appspot.com",
+    messagingSenderId: "56900868593",
+    appId: "1:56900868593:web:f5c53babb948e6486f0c50"
+  };
